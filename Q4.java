8af52b6087f8bcfd8a161d7530c78f986804f65e
Tom Goureau <116594788+TomGoureau@users.noreply.github.com>
2024-11-20 14:28:56 +0100

    Create Q4.java
    le .java

---
+++ Q4.java
@@ -0,0 +1,15 @@
+public class Q4
+{
+	public static void main(String[] arg)
+	{
+    String s="";
+    int cpt=0;
+		while (cpt < Integer.parseInt(arg[0]) )
+		{
+			s+= '*';
+		  System.out.println( s );
+			cpt++ ;
+		}
+		
+	}
+}
